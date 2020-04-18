37249df7618c993f8c72afb6402827e56bf57100
noku-lab <61013631+noku-lab@users.noreply.github.com>
2020-04-18 20:46:42 +0200

    Updates

---
+++ precache-manifest.e9e0b18e7e43f17e7a198fed01ad10f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04585e7747d18fb7142f5962d7e8184a",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "0ded3a558c7731376620",
+    "url": "/covid-tracker/static/css/main.8c200a63.chunk.css"
+  },
+  {
+    "revision": "75d775620b70cf4392ee",
+    "url": "/covid-tracker/static/js/2.8d91aed7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.8d91aed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ded3a558c7731376620",
+    "url": "/covid-tracker/static/js/main.3e9524f0.chunk.js"
+  },
+  {
+    "revision": "8763f2d5b80f99c27159",
+    "url": "/covid-tracker/static/js/runtime-main.aa6326f9.js"
+  }
+]);
